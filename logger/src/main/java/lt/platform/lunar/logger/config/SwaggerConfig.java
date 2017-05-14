0d9830c1f865d2bdcff6a452df5e0392828215a8
Mantas Domaševičius <mantas.domasevicius@funguide.lt>
2017-05-14 09:39:28 +0300

    Fix swagger apis package

--- logger/src/main/java/lt/platform/lunar/logger/config/SwaggerConfig.java
+++ logger/src/main/java/lt/platform/lunar/logger/config/SwaggerConfig.java
@@ -16,7 +16,7 @@ class SwaggerConfig {
     Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
             .select()
-            .apis(RequestHandlerSelectors.basePackage("lt.lunar.platform"))
+            .apis(RequestHandlerSelectors.basePackage("lt.platform.lunar"))
             .paths(PathSelectors.any())
             .build();
     }
